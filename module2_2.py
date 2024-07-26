d030a4a8aca781b90f65378630a1399db1518f52
Riji-ai <riji.dav@yandex.ru>
2024-07-26 20:48:12 +0300

    Add files via upload

---
+++ module2_2.py
@@ -0,0 +1,10 @@
+first = 8
+second = 5
+third = 9
+number = int(input('Введите число: '))
+if number == first:
+    print(3)
+if number ==  first and second:
+    print(2)
+if number == first or second or third:
+    print(0)
